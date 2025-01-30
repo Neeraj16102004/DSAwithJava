24b509f06fcd0e3d4f1e441aa6516271b86b7180
Neeraj16102004 <neerajrananr338@gmail.com>
2025-01-30 19:50:08 +0530

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import DataStructures.DoubleyLinkedList;
+import DataStructures.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       //LinkedList
+      //  LinkedList myList = new LinkedList(4);
+
+      //  myList.getHead();
+      //  myList.append(3);
+       // myList.printList();
+     //   myList.removeLast();
+     //   myList.printList();
+     //   myList.prepend(1);
+      //  myList.printList();
+       // myList.removeFirst();
+       // myList.printList();
+
+        //DoubleyLinkedList
+        DoubleyLinkedList myList1 = new DoubleyLinkedList(4);
+        myList1.printList();
+        System.out.println(" ");
+        myList1.append(6);
+        myList1.append(5);
+        myList1.printList();
+      //  System.out.println(myList1.removeLast());
     }
 }
